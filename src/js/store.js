db850bdd5da177e1c1c17f4fcb7b82244c9affd7
MDonald1 <m.m.donald@outlook.com>
2018-02-19 11:16:51 +0000

    Add reducer for handling gameplay

--- src/js/store.js
+++ src/js/store.js
@@ -1,15 +1,17 @@
 import { createStore, combineReducers } from 'redux'
 import common from './reducers/common'
+import gameplayReducer from './reducers/gameplay'
 
 const reducer = combineReducers({
   common,
+  gameplayReducer,
 })
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
   /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 /* eslint-enable */
 
