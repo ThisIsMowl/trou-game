0007b913634e25bc1c32fba44f4ec47e56a5d28c
MDonald1 <m.m.donald@outlook.com>
2018-02-22 10:14:19 +0000

    Rework PlayingRow

--- src/js/components/gamePieces/PlayingRow.js
+++ src/js/components/gamePieces/PlayingRow.js
@@ -4,20 +4,14 @@ import PieceHolder from './../gamePieces/PieceHolder'
 import SelectionPiece from './../gamePieces/SelectionPiece'
 
 
-const PlayingRow = (props) => {
-  const {
-    currentPiece,
-  } = props
-
-  return (
-    <div className="columns-holder">
-      {[1, 1, 1, 1, 1, 1, 1].map((x, i) => (
-        <PieceHolder noBorder>
-          <SelectionPiece activePlayer={props.activePlayer} id={i} active={i === currentPiece} />
-        </PieceHolder>
-      ))}
-    </div>
-  )
-}
+const PlayingRow = props => (
+  <div className="columns-holder">
+    {[1, 1, 1, 1, 1, 1, 1].map((x, i) => (
+      <PieceHolder noBorder>
+        <SelectionPiece activePlayer={props.activePlayer} id={i} />
+      </PieceHolder>
+    ))}
+  </div>
+)
 
 export default PlayingRow
