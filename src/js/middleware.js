a369b7bb7350b1d998935e0a9d2da7a0dcc339a0
MDonald1 <m.m.donald@outlook.com>
2018-02-27 09:09:54 +0000

    Annotations in middleware + remove full variable

--- src/js/middleware.js
+++ src/js/middleware.js
@@ -1,26 +1,31 @@
 import keyTypes from './keyTypes'
 import common from './actions/common'
 
+
+// Middleware for handling CHANGE_TARGET_COLUMN action.
 const boardCheck = store => next => (action) => {
   if (action.type === keyTypes.CHANGE_TARGET_COLUMN) {
-    const blankIndex = action.column.lastIndexOf(0)
-    store.dispatch(common.changeTargetField(action.payload, blankIndex))
+    /* find the last empty spot in the column.
+    This is where our counter will land if we drop it. */
+    const i = action.column.lastIndexOf(0)
+    // Send our target spot to the state using the CHANGE_TARGET_FIELD action
+    store.dispatch(common.changeTargetField(action.payload, i))
   }
   next(action)
 }
 
+// Middleware for handling the 'DROP_COUNTER' action
 const dropCounterMiddleware = store => next => (action) => {
   if (action.type === keyTypes.DROP_COUNTER) {
+    // Do not add another counter if the column is full.
     if (action.y !== -1) {
       const { currentPlayer } = store.getState().common
       let { boardState } = store.getState().common
 
+      // Reasign blank target field with counter.
       boardState[action.x][action.y] = currentPlayer
 
       action.payload = boardState
-    } else {
-      action.full = true
-      console.log('full')
     }
   }
   next(action)
