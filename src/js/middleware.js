9bca94e852ee1e3a60f5ad49e87fdd51aa00b9d7
MDonald1 <m.m.donald@outlook.com>
2018-02-28 10:03:17 +0000

    Complete and add middleware for checking winstate

--- src/js/middleware.js
+++ src/js/middleware.js
@@ -1,6 +1,47 @@
 import keyTypes from './keyTypes'
 import common from './actions/common'
 
+// Function which checks if there is a win state.
+const checkWinState = (gameBoard, playerNo) => {
+  let count = 0
+  // Check horizontally
+
+  for (let i = 0; i < 7; i += 1) {
+    for (let j = 0; j < 6; j += 1) {
+      count = (gameBoard[i][j] === playerNo) ? count + 1 : 0
+      if (count === 4) return true
+    }
+  }
+
+  // Check vertically
+  for (let j = 0; j < 6; j += 1) {
+    for (let i = 0; i < 7; i += 1) {
+      count = (gameBoard[i][j] === playerNo) ? count + 1 : 0
+      if (count === 4) return true
+    }
+  }
+
+  // Check diagonally
+  // Part 1: Bottom lines
+
+  for (let rowStart = 0; rowStart < (6 - 4); rowStart += 1) {
+    for (let row = rowStart, col = 0; row < 6 && col < 7; row += 1, col += 1) {
+      count = (gameBoard[col][row] === playerNo) ? count + 1 : 0
+      if (count === 4) return true
+    }
+  }
+
+  // Part 2: Top lines
+
+  for (let colStart = 1; colStart < (7 - 4); colStart += 1) {
+    for (let row = 0, col = colStart; row < 6 && col < 7; row += 1, col += 1) {
+      count = (gameBoard[col][row] === playerNo) ? count + 1 : 0
+      if (count === 4) return true
+    }
+  }
+
+  return false
+}
 
 // Middleware for handling CHANGE_TARGET_COLUMN action.
 const boardCheck = store => next => (action) => {
@@ -25,33 +66,17 @@ const dropCounterMiddleware = store => next => (action) => {
       // Reasign blank target field with counter.
       boardState[action.x][action.y] = currentPlayer
       action.payload = boardState
-      // Change player
-      store.dispatch(common.changePlayer())
+
+      // Check if there is a winner
+      action.winner = checkWinState(boardState, currentPlayer)
+
+      if (!action.winner) {
+        // Change player
+        store.dispatch(common.changePlayer())
+      }
     }
   }
   next(action)
-}
-
-const checkWinState = (gameBoard, playerNo) => {
-  let count = 0
-  // Check horizontally
-
-  for (let i = 0; i < 7; i++) {
-    for(let j = 0; j < 6; j++) {
-      count = (gameBoard[i][j] === playerNo) ? count + 1 : 0
-      if (count === 4) {} //win condition
-    }
-  }
-
-  // Check vertically
-  for (let j = 0; j < 6; j++) {
-    for(let i = 0; i < 7; i++) {
-      count = (gameBoard[i][j] === playerNo) ? count + 1 : 0
-      if (count === 4) {} //win condition
-    }
-  }
-
-  // Check diagonally
 }
 
 export default {
