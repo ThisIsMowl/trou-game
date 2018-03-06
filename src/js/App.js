7f7b8fc953122c45cf07bcbcd9d9f2a8324b7fc1
MDonald1 <m.m.donald@outlook.com>
2018-03-06 09:59:45 +0000

    Add Header component

--- src/js/App.js
+++ src/js/App.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/App.css'
 
 import GameplayContainer from './components/GameplayContainer'
+import Header from './components/Header'
 
 const App = () => (
   <div className="App">
     <div className="container">
+      <Header />
       <GameplayContainer />
     </div>
   </div>
---
+++ src/js/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="game-header">
+    <h1 className="center-text">Trou Game</h1>
+    <p>
+      Here is the game I was talking about.
+    </p>
+  </div>
+)
+
+export default Header
