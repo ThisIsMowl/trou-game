e78128f2cb96b6e1f138b90521b8dd3d7b404972
MDonald1 <15128075+MDonald1@users.noreply.github.com>
2019-01-30 09:16:40 +0000

    Can restart using keyboard shortcut

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Hotkeys from 'react-hot-keys'
+
+import common from './actions/common'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/App.css'
@@ -6,15 +10,43 @@ import '../css/App.css'
 import GameplayContainer from './components/GameplayContainer'
 import Header from './components/Header'
 
-const App = () => (
-  <div className="App">
-    <div className="game-holder">
-      <div className="content">
-        <Header />
-        <GameplayContainer />
-      </div>
-    </div>
-  </div>
-)
+const mapDispatch = dispatch => ({
+  restartGame: () =>
+    dispatch(common.restartGame()),
+})
 
-export default App
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.restartGame = () => {
+      this.props.restartGame()
+    }
+  }
+
+  onKeyDown(keyName, e, handle) {
+    e.preventDefault()
+    this.restartGame()
+  }
+
+
+  render() {
+    return (
+      <Hotkeys
+        keyName="ctrl+a,command+a"
+        onKeyDown={this.onKeyDown.bind(this)}
+      >
+        <div className="App">
+          <div className="game-holder">
+            <div className="content">
+              <Header />
+              <GameplayContainer />
+            </div>
+          </div>
+        </div>
+      </Hotkeys>
+    )
+  }
+}
+
+export default connect(() => ({}), mapDispatch)(App)
